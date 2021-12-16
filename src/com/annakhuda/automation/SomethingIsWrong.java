d896f71bcc8ff6a46f3356c8ccd0b298979e34e2
Anna Yadelska <anna_yadelska@epam.com>
2021-12-16 16:53:58 +0200

    test commit

--- src/com/annakhuda/automation/SomethingIsWrong.java
+++ src/com/annakhuda/automation/SomethingIsWrong.java
@@ -5,7 +5,7 @@ public class SomethingIsWrong {
     public static void main(String[] args) {
         Rectangle myRect = new Rectangle();
         myRect.setWidth(40);
-        myRect.setHeight(50);
+        myRect.setHeight(60);
         System.out.println("myRect's area is " + myRect.area());
     }
 }
